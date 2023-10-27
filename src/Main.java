a8fa1fae08650058c7b3faf2985c64c320cb7400
DuyLeVu <levuduy132@gmail.com>
2023-10-27 16:02:04 +0700

    init project

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+//    Start receiving messages - ready to receive messages
+
+//    Handle a new incoming message
+//    Read the request - listen to the message
+//    Decide how we'd like to response
+        // Just send back a simple "Hello world"
+        // Send back an image ?
+        // Change response based on route ?
+//    Send a response - send our reply
+
+//   Get ready for the next message
+  }
+
+}
